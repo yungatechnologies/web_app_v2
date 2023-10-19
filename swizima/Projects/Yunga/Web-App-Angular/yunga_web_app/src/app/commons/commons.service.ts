d62b0383951ee3f93cad3417034ef3b1bf7d6199
root <kfredrick35@gmail.com>
2023-10-19 08:07:26 +0300

    added firebase db connection

--- swizima/Projects/Yunga/Web-App-Angular/yunga_web_app/src/app/commons/commons.service.ts
+++ swizima/Projects/Yunga/Web-App-Angular/yunga_web_app/src/app/commons/commons.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { DocumentData, Firestore, QuerySnapshot, addDoc, collection, getDocs, query } from '@angular/fire/firestore';
+import { DocumentData, DocumentReference, Firestore, QuerySnapshot, addDoc, collection, getDocs, query } from '@angular/fire/firestore';
 import { Observable, flatMap, from, map, mergeMap } from 'rxjs';
 
 @Injectable({
@@ -14,6 +14,23 @@ export class CommonsService {
 
         return from(getDocs(query(collection(this.firestore, collectionKey)))).pipe(map(querySnaphot => querySnaphot.docs.map(D => D.data())))
 
+    }
+
+    async addDocument(collectionKey: string, data: any) {
+
+        console.log("adding Document ", data);
+
+        //let ref:DocumentReference<DocumentData> = await addDoc(collection(this.firestore, "collectionKey"),data);
+
+        const docRef = await addDoc(collection(this.firestore, collectionKey), {
+            name: "Fredrirk testing",
+            color: "Grean",
+            age: 5
+        });
+
+        console.log("Document written with ID: ", docRef.id);
+
+        //console.log(ref);
     }
 
 }
--- swizima/Projects/Yunga/Web-App-Angular/yunga_web_app/src/app/customers/popups/customer-popup.component.ts
+++ swizima/Projects/Yunga/Web-App-Angular/yunga_web_app/src/app/customers/popups/customer-popup.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { CommonsService } from 'src/app/commons/commons.service';
 
 @Component({
     selector: 'app-customer-popup',
@@ -10,21 +11,27 @@ export class CustomerPopupComponent implements AfterViewInit {
 
 
 
-    constructor(private formBuilder:FormBuilder) {
+    constructor(private formBuilder: FormBuilder, private commonsService: CommonsService) {
 
     }
 
-    myform=this.formBuilder.group({
-        surname:this.formBuilder.control(''),
-        givenName:this.formBuilder.control(''),
-        phone:this.formBuilder.control(''),
-        email:this.formBuilder.control(''),
-        address:this.formBuilder.control(''),
+    myform = this.formBuilder.group({
+        surname: this.formBuilder.control(''),
+        givenName: this.formBuilder.control(''),
+        phone: this.formBuilder.control(''),
+        email: this.formBuilder.control(''),
+        address: this.formBuilder.control(''),
 
     });
 
-    save(){
-        
+    save() {
+        //console.log(this.myform.value);
+        console.log("Saving data");
+        this.commonsService.addDocument("testMessaging", {
+            surname: this.myform.value.surname,
+            givenName: this.myform.value.givenName,
+            phone: this.myform.value.phone,
+        });
     }
 
     ngAfterViewInit(): void {
